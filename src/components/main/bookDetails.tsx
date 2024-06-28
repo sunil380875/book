e5c365677041f6314afeaa97fd5b0989beba4b53
sunil380875 <sunil.crud@gmail.com>
2024-06-28 12:48:57 +0530

    book details page

--- src/components/main/bookDetails.tsx
+++ src/components/main/bookDetails.tsx
@@ -1,22 +1,41 @@
+import { useState } from "react";
 import Heading from "./heading";
-import { CiCirclePlus } from "react-icons/ci";
-import { CiCircleMinus } from "react-icons/ci";
+import { CiCirclePlus, CiCircleMinus } from "react-icons/ci";
+import Paragraph from "./paragraph";
+
 export default function BookDetails() {
+  const [zoom, setZoom] = useState(1);
+
   return (
     <>
-      <section className="flex text-3xl justify-end px-32 mt-10 gap-3 ">
-        <div className="flex border px-2 py-2">
-          <div className="cursor-pointer text-green-400">
+      {/* Sidebar */}
+      <div
+        className={`fixed top-0 left-0 h-full w-96 bg-sky-200 z-20 transform transition-transform ease-in-out `}
+      ></div>
+      {/* <section className="flex text-3xl justify-end items-center px-32 mt-10 gap-3">
+        <div className="flex px-36 my-10 justify-center items-center">
+          <Heading text="This is a Good Heading" css={zoom} />
+        </div>
+        <div className="flex">
+          <div
+            className="cursor-pointer text-green-400"
+            onClick={() => setZoom(zoom + 1)}
+          >
             <CiCirclePlus />
           </div>
-          <div className="cursor-pointer text-blue-200">
+          <div
+            className="cursor-pointer text-blue-200"
+            onClick={() => setZoom(zoom - 1)}
+          >
             <CiCircleMinus />
           </div>
         </div>
-      </section>
-      <section className="flex px-36 my-10 justify-center items-center">
-        <Heading props="This is a Good Heading" />
-      </section>
+      </section> */}
+      <div className="flex border flex-col px-32 h-screen">
+        <section className="flex px-36 my-10 justify-center items-center">
+          <Paragraph text="This is a Good Paragraph" css={zoom} />
+        </section>
+      </div>
     </>
   );
 }
--- src/components/main/paragraph.tsx
+++ src/components/main/paragraph.tsx
@@ -1,8 +1,8 @@
-export default function Heading({ text, css }: any) {
+export default function Paragraph({ text, css }: any) {
   return (
     <div
       className={`px-20 font-bold text-center `}
-      style={{ fontSize: `${css + 50}px` }}
+      style={{ fontSize: `${css + 30}px` }}
     >
       <p>{text}</p>
     </div>
