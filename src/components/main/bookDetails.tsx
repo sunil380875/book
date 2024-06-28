6e9920477d6a69b991b6c770f43816906c3eb0a5
sunil380875 <sunil.crud@gmail.com>
2024-06-28 13:00:21 +0530

    separate

--- src/components/main/bookDetails.tsx
+++ src/components/main/bookDetails.tsx
@@ -8,34 +8,15 @@ export default function BookDetails() {
 
   return (
     <>
-      {/* Sidebar */}
-      <div
-        className={`fixed top-0 left-0 h-full w-96 bg-sky-200 z-20 transform transition-transform ease-in-out `}
-      ></div>
-      {/* <section className="flex text-3xl justify-end items-center px-32 mt-10 gap-3">
-        <div className="flex px-36 my-10 justify-center items-center">
-          <Heading text="This is a Good Heading" css={zoom} />
+      <section className="flex">
+        <div className="fixed top-0 left-0 h-full w-[25%] bg-sky-200">
+          
         </div>
-        <div className="flex">
-          <div
-            className="cursor-pointer text-green-400"
-            onClick={() => setZoom(zoom + 1)}
-          >
-            <CiCirclePlus />
-          </div>
-          <div
-            className="cursor-pointer text-blue-200"
-            onClick={() => setZoom(zoom - 1)}
-          >
-            <CiCircleMinus />
-          </div>
+
+        <div className="fixed top-0 right-0 w-[75%] h-full bg-pink-200">
+
         </div>
-      </section> */}
-      <div className="flex border flex-col px-32 h-screen">
-        <section className="flex px-36 my-10 justify-center items-center">
-          <Paragraph text="This is a Good Paragraph" css={zoom} />
-        </section>
-      </div>
+      </section>
     </>
   );
 }
